3ba8e81ed0660f7d0ed8ca31812614943b3d3958
RFlor14 <57841815+RFlor14@users.noreply.github.com>
2021-02-02 02:30:24 -0500

    Added CoreEngine getInstance and Run function in Main.Cpp

--- GameEngine/Main.cpp
+++ GameEngine/Main.cpp
@@ -1,27 +1,30 @@
-#include "Engine/Core/CoreEngine.h" // self explanatory, allows acess of entire window class
+#include "Engine/Core/CoreEngine.h" // self explanatory, allows acess of entire Engine class
 
-CoreEngine* engine; // creates global engine pointer
-
-// THIS IS JUST FOR TESTING PURPOSES
+// Main Is now very basic, and will not be changed anymore in the forseeable future.
 int main(int argc, char* argv[]) 
 {
-	engine = new CoreEngine(); // Initalizes engine variable
 
-	// Sets name, width, and height of the window, seen in the parameters.
-	// Again, just error checking, if everything fails clean memory by deleting window, and stop the function.
-	if (!engine->OnCreate("Game 258 Engine", 800, 600)) 
+	/*
+	Sets name, width, and height of the window, seen in the parameters.
+	gain, just error checking, if everything fails clean memory by deleting window, and stop the function.
+
+	All it does now is it creates the game and engine. It then tell's the engine to go.
+
+	Now we need call get instance first.
+
+	GetInstance: 
+	Is a function, () needed.
+	Returns a CoreEngine pointer, -> needed, to acess any variables that are public.
+	*/
+	if (!CoreEngine::GetInstance()->OnCreate("Game 258 Engine", 800, 600)) 
 	{
-		delete engine;
-		engine = nullptr;
+		std::cout << "Engine failed to initialize" << std::endl;
 		return 0;
 	}
 
-	engine->Run();
-
-	// Afterwards, clean everything
-	delete engine;
-	engine = nullptr;
+	CoreEngine::GetInstance()->Run(); // Once engine is created, call run function.
 	return 0;
+
 
 }
 
