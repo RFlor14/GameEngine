01aaefb7d424f5f758ce559bc37c41e9e013b863
RFlor14 <57841815+RFlor14@users.noreply.github.com>
2021-02-02 01:53:13 -0500

    Added CoreEngine.cpp Fixed Main.cpp

--- GameEngine/Engine/Core/CoreEngine.cpp
+++ GameEngine/Engine/Core/CoreEngine.cpp
@@ -1,12 +1,99 @@
 #include "CoreEngine.h"
 
 
+/* 
+Sets default value for window.
+Makes sure that the engine is(NOT)Running.
+*/
+CoreEngine::CoreEngine() : window(nullptr), isRunning(false) {} 
 
-CoreEngine::CoreEngine()
+CoreEngine::~CoreEngine() {}
+
+// Has the same parameters as Windows onCreate since it does call that function.
+bool CoreEngine::OnCreate(std::string name_, int width_, int height_)
 {
+	//Before calling onCreate, makes sure that we are creating and initializing the window pointer.
+	window = new Window();
+
+	/*
+	Calls window's onCreate function, check's what the return value of the function is.
+
+	!window means that if it returns false, go through the if statement.
+	Which basically prints out an error on console, cleans window.
+	
+	return isRunning does two things:
+	1) Sets isRunning to false. 
+	2) Returns the value of isRunning var.
+	*/
+	if (!window->OnCreate(name_, width_, height_))
+	{
+		std::cout << "Window failed to initialize" << std::endl;
+		OnDestroy();
+		return isRunning = false;
+	}
+
+	// If everything goes well, engines onCreate sets isRunning var to true.
+	return isRunning = true;
 }
 
+/* 
+Entire game loop of the engine. 
+The game loop is "Update, render, update render" - Scott
 
-CoreEngine::~CoreEngine()
+[Key Info]
+Update goes first because anything that involves physics (like motion & collision)
+happens inside of the update function. 
+
+Whenever something needs to be rendered, it makes sure that it's rendering w/ most
+up to date information and date positions.
+
+
+*/
+void CoreEngine::Run()
 {
+
+	while (isRunning)
+	{
+		Update(0.016f); // Updates in 60fps (1/60 = 0.0167).
+		Render();
+	}
+
+	/* 
+	Makes it clear when on destroy is being called (the if statement is optional
+	as it does the same thing w/o it.
+
+	if isRunning = false, jump outside of while loop from Run function,
+	and go to OnDestroy function.
+	*/
+	if (!isRunning)
+	{
+		OnDestroy();
+	}
+
+}
+
+bool CoreEngine::GetIsRunning()
+{
+	return isRunning;
+}
+
+void CoreEngine::Update(const float deltaTime_) {}
+
+void CoreEngine::Render()
+{
+	// Same thing from Main.cpp
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Tell's OpenGL what color should be used when cleared. (R,G,B,A)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Tell's OpenGL to clear Color and Depth buffer bit.
+
+	//CALL GAME RENDER
+	SDL_GL_SwapWindow(window->GetWindow());
+}
+
+// Deletes window variable
+void CoreEngine::OnDestroy()
+{
+	delete window; // cleans up the pointer
+	window = nullptr; // then set it to nullptr
+	SDL_Quit(); // Quits SDL, when destroying an engine, close entire project.
+	exit(0); // Exits whole program w/ exit code 0.
 }
--- GameEngine/Main.cpp
+++ GameEngine/Main.cpp
@@ -1,30 +1,26 @@
-#include "Engine/Core/Window.h" // self explanatory, allows acess of entire window class
+#include "Engine/Core/CoreEngine.h" // self explanatory, allows acess of entire window class
 
-Window* window; // creates global window pointer
+CoreEngine* engine; // creates global engine pointer
 
 // THIS IS JUST FOR TESTING PURPOSES
 int main(int argc, char* argv[]) 
 {
-	window = new Window(); // calls in OnCreate constructor
+	engine = new CoreEngine(); // Initalizes engine variable
 
 	// Sets name, width, and height of the window, seen in the parameters.
 	// Again, just error checking, if everything fails clean memory by deleting window, and stop the function.
-	if (!window->OnCreate("Game 258 Engine", 800, 600)) 
+	if (!engine->OnCreate("Game 258 Engine", 800, 600)) 
 	{
-		delete window;
-		window = nullptr;
+		delete engine;
+		engine = nullptr;
 		return 0;
 	}
 
-	glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Tell's OpenGL what color should be used when cleared. (R,G,B,A)
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Tell's OpenGL to clear Color and Depth buffer bit.
-	// >-< this line is where rendering happens.
-	SDL_GL_SwapWindow(window->GetWindow());  
-	SDL_Delay(5000); // How long the window's chilling in the screen (5s)
+	engine->Run();
 
 	// Afterwards, clean everything
-	delete window;
-	window = nullptr;
+	delete engine;
+	engine = nullptr;
 	return 0;
 
 }
