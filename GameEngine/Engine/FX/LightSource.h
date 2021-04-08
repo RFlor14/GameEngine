d3efee99812c73b0109cb0b345c737fa7e5b44b6
RFlor14 <57841815+RFlor14@users.noreply.github.com>
2021-04-08 18:50:55 -0400

    Added LightSource.h and FragmentShader variables

--- GameEngine/Engine/FX/LightSource.h
+++ GameEngine/Engine/FX/LightSource.h
@@ -1,5 +1,42 @@
+#ifndef LIGHTSOURCE_H
+#define LIGHTSOURCE_H
+
+#include <glm/glm.hpp>
 
 class LightSource
 {
+public:
+
+	LightSource(glm::vec3 lightPos_ = glm::vec3(),
+		float ambient_ = 0.0f,
+		float diffuse_ = 0.0f,
+		float specular_ = 0.0f,
+		glm::vec3 lightCol_ = glm::vec3(0.0f, 1.0f, 0.0f));
+
+	~LightSource();
+
+	glm::vec3 GetLightPos() const;
+	float GetAmbient() const;
+	float GetDiffuse() const;
+	float GetSpecular() const;
+	glm::vec3 GetLightCol() const;
+
+
+	void SetLightPos(glm::vec3 lightPos_);
+	void SetAmbient(float ambient_);
+	void SetDiffuse(float diffuse_);
+	void SetSpecular(float specular_);
+	void SetLightCol(glm::vec3 lightCol_);
+
+private:
+	glm::vec3 lightPos;
+	float ambient;
+	float diffuse;
+	float specular;
+	glm::vec3 lightCol;
 };
+
+
+#endif LIGHTSOURCE_H
+
 
--- GameEngine/Engine/Shaders/FragmentShader.glsl
+++ GameEngine/Engine/Shaders/FragmentShader.glsl
@@ -9,6 +9,16 @@ uniform sampler2D inputTexture;
 
 out vec4 fColour;
 
+// Added light struct (week 7)
+struct Light
+{
+	vec3 lightPos;
+	float ambient;
+	float diffuse;
+	float specular;
+	vec3 lightCol;
+};
+
 void main()
 {
 	fColour = texture(inputTexture, TexCoords);
