2f8a87d4832dacc0ed11253844f0a4f2e7ef003c
RFlor14 <57841815+RFlor14@users.noreply.github.com>
2021-04-14 21:50:20 -0400

    Finished LoadOBJModel.cpp

---
+++ GameEngine/Engine/Rendering/3D/LoadOBJModel.cpp
@@ -0,0 +1,34 @@
+#include "LoadOBJModel.h"
+
+LoadOBJModel::LoadOBJModel()
+{
+}
+
+LoadOBJModel::~LoadOBJModel()
+{
+}
+
+void LoadOBJModel::LoadModel(const std::string& objFilePath_, const std::string& mtlFilePath_)
+{
+}
+
+std::vector<SubMesh> LoadOBJModel::GetSubMeshes()
+{
+	return std::vector<SubMesh>();
+}
+
+void LoadOBJModel::PostProcessing()
+{
+}
+
+void LoadOBJModel::LoadModel(const std::string& filePath_)
+{
+}
+
+void LoadOBJModel::LoadMaterial(const std::string& matName_)
+{
+}
+
+void LoadOBJModel::LoadMaterialLibrary(const std::string& matFilePath_)
+{
+}
---
+++ GameEngine/Engine/Rendering/3D/LoadOBJModel.h
@@ -0,0 +1,52 @@
+#ifndef LOADOBJMODEL_H
+#define LOADOBJMODEL_H
+
+#include <sstream>
+#include "Mesh.h"
+#include "../../Graphics/TextureHandler.h"
+
+class LoadOBJModel
+{
+public:
+	LoadOBJModel();
+	~LoadOBJModel();
+
+	// Takes in path for Obj and mtl.
+	void LoadModel(const std::string& objFilePath_,
+		const std::string& mtlFilePath_);
+
+	std::vector<SubMesh> GetSubMeshes();
+
+private:
+
+	/*
+	 Once we're done looking at single meshes, do post processing work
+	 to get all the data from OBJ into the sub meshes object.
+	*/
+	void PostProcessing();
+
+	// Looks at the obj file and getting all the data out of it.
+	void LoadModel(const std::string& filePath_);
+
+	// Goes to texture handler and creating the texture if it's not created.
+	void LoadMaterial(const std::string& matName_);
+
+	/*
+	 Opens mtl file, get specific mat that needs to load, 
+	 then call load material function.
+	*/ 
+	void LoadMaterialLibrary(const std::string& matFilePath_);
+
+	
+	std::vector<glm::vec3> vertices; // Holds 3d vectors
+	std::vector<glm::vec3> normals;
+	std::vector<glm::vec2> textureCoords; // 2d vectors
+	std::vector<unsigned int> indices, normalIndices, textureIndices; // deals w/ face data
+	std::vector<Vertex> meshVertices; 
+	std::vector<SubMesh> subMeshes;
+
+	GLuint currentTexture;
+};
+
+#endif LOADOBJMODEL_H
+
