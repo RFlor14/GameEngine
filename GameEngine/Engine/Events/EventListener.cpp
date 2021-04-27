54b5772c2f1fce029cbb11b641a505ae784e702b
RFlor14 <57841815+RFlor14@users.noreply.github.com>
2021-04-27 17:10:44 -0400

    Added functions to EventListener.cpp/h

--- GameEngine/Engine/Events/EventListener.cpp
+++ GameEngine/Engine/Events/EventListener.cpp
@@ -1,1 +1,52 @@
 #include "EventListener.h"
+#include "../Core/CoreEngine.h"
+
+EventListener::~EventListener()
+{
+}
+
+// Called Every frame
+void EventListener::Update()
+{
+	/*
+	 Ever frame, create a new sdl event.
+
+	 Then, poll the events that are
+	 currently happening in this instance
+	 of the engine.
+	*/
+	SDL_Event sdlEvent;
+	while (SDL_PollEvent(&sdlEvent))
+	{
+		/*
+		 First, check that, if the event type 
+		 is a quit, call the engines exit
+		 function.
+		*/
+		if (sdlEvent.type == SDL_QUIT)
+		{
+			CoreEngine::GetInstance()->Exit();
+		}
+
+		/*
+		 If it's not a quite type, go into the
+		 switch case statement.
+
+		 Cases are listed one after the other
+		 because if this value of this type is either
+		 any of these, then call in mouse event's
+		 update function.
+		*/
+		switch (sdlEvent.type)
+		{
+		case SDL_MOUSEBUTTONDOWN:
+		case SDL_MOUSEBUTTONUP:
+		case SDL_MOUSEMOTION:
+		case SDL_MOUSEWHEEL:
+			MouseEventListener::Update(sdlEvent);
+			break;
+		default:
+			break;
+		}
+	}
+}
--- GameEngine/Engine/Events/EventListener.h
+++ GameEngine/Engine/Events/EventListener.h
@@ -1,5 +1,44 @@
-#pragma once
+#ifndef EVENTLISTENER_H
+#define EVENTLISTENER_H
+
+#include "MouseEventListener.h"
+
+/*
+	EventListener is seperated to 
+	MouseEventListener since it
+	essentially listens to all
+	types of events. 
+
+	EventListener helps with distinction
+	of other types of events. Ex, if
+	we do a mouse event, it goes to
+	the mouse event listener class, if 
+	it's keyboard go to keyboard.	
+*/
+
 class EventListener
 {
+public:
+
+	// Purely static class
+	EventListener(const EventListener&) = delete;
+	EventListener(EventListener&&) = delete;
+	EventListener& operator=(const EventListener&) = delete;
+	EventListener& operator=(EventListener&&) = delete;
+
+	EventListener() = delete;
+	~EventListener();
+
+	/*
+	 This update is not a normal update, it's
+	 a special event listener update.
+	*/
+	static void Update();
+
 };
+
+#endif // !EVENTLISTENER_H
+
+
+
 
