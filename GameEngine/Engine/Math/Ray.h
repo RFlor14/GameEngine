c9ee7f040bbbc78ea8e58c18d0e6e94e1291d70a
RFlor14 <57841815+RFlor14@users.noreply.github.com>
2021-04-27 21:45:45 -0400

    Finished functions for Ray.h + added CollisionDetection

---
+++ GameEngine/Engine/Math/Ray.h
@@ -0,0 +1,63 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include <glm/glm.hpp>
+#include "BoundingBox.h"
+
+struct Ray 
+{
+	glm::vec3 origin;
+	glm::vec3 direction;
+
+	/*
+	 Help us calculate the collision
+	 detection and what specific object
+	 was hit.
+	*/
+	float intersectionDist;
+
+	// Default constructor
+	inline Ray()
+	{
+		origin = glm::vec3();
+		direction = glm::vec3();
+		intersectionDist = 0.0f;
+	}
+
+	inline Ray(glm::vec3 origin_, glm::vec3 direction_)
+	{
+		origin = origin_;
+		direction = direction_;
+		intersectionDist = 0.0f;
+	}
+
+	/*
+	 Operator overload, way to define special things
+	 that you want to do for operator.
+
+	 In this case we're creating an operator overload
+	 for this equal sign. 1 = means its an assignment
+	 operator.
+	*/
+	inline Ray& operator =(const Ray& ray_)
+	{
+		origin = ray_.origin;
+		direction = ray_.direction;
+		intersectionDist = 0.0f;
+	}
+
+	/*
+	 Used to really check collision detection with
+	 a ray and bounding box.
+	*/
+	inline bool IsColliding(BoundingBox* box_)
+	{
+		intersectionDist = -1.0f;
+		return true;
+	}
+
+};
+
+#endif // !RAY_H
+
+ 
