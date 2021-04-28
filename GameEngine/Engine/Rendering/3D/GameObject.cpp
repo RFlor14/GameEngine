549f9ef03da766d71c17f366e53c0550f1d7e154
RFlor14 <57841815+RFlor14@users.noreply.github.com>
2021-04-27 23:26:21 -0400

    Created a Get/Set hit function in GameObject.cpp/h

--- GameEngine/Engine/Rendering/3D/GameObject.cpp
+++ GameEngine/Engine/Rendering/3D/GameObject.cpp
@@ -7,7 +7,7 @@ Sets class' varable model, equal to model that we
 pass in as a parameter.
 */
 GameObject::GameObject(Model* model_, glm::vec3 position_) : model(nullptr), position(glm::vec3()),
-angle(0.0f), rotation(glm::vec3(0.0f, 1.0f, 0.0f)), scale(glm::vec3(1.0f)), modelInstance(0)
+angle(0.0f), rotation(glm::vec3(0.0f, 1.0f, 0.0f)), scale(glm::vec3(1.0f)), modelInstance(0), hit(false)
 {
 	model = model_;
 	position = position_;
@@ -78,6 +78,11 @@ BoundingBox GameObject::GetBoundingBox() const
 	return boundingBox;
 }
 
+bool GameObject::GetHit() const
+{
+	return hit;
+}
+
 void GameObject::SetPosition(glm::vec3 position_)
 {
 	position = position_;
@@ -123,4 +128,13 @@ void GameObject::SetScale(glm::vec3 scale_)
 void GameObject::SetTag(std::string tag_)
 {
 	tag = tag_;
+}
+
+void GameObject::SetHit(bool hit_, int buttonType)
+{
+	hit = hit_;
+	if (hit)
+	{
+		std::cout << tag << " was hit" << std::endl;
+	}
 }
--- GameEngine/Engine/Rendering/3D/GameObject.h
+++ GameEngine/Engine/Rendering/3D/GameObject.h
@@ -25,12 +25,14 @@ public:
 	glm::vec3 GetScale() const;
 	std::string GetTag() const;
 	BoundingBox GetBoundingBox() const;
+	bool GetHit() const;
 
 	void SetPosition(glm::vec3 position_);
 	void SetAngle(float angle_);
 	void SetRotation(glm::vec3 rotation_);
 	void SetScale(glm::vec3 scale_);
 	void SetTag(std::string tag_);
+	void SetHit(bool hit_, int buttonType);
 
 private:
 
@@ -50,6 +52,8 @@ private:
 	std::string tag;
 
 	BoundingBox boundingBox;
+
+	bool hit;
 };
 
 #endif // !GAMEOBJECT_H
